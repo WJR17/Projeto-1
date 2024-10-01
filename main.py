4a023a440409263ef01140183f7b15db2ca7919d
WJR17 <william.junior.ifpr@gmail.com>
2024-10-01 16:41:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Projeto 1
+William Junior
+2023.05.24
+"""
+
+palavras = ["barça","city","inter","juve","milan"]
+print(palavras[2]) 
+i = 0
+for i in range(len(palavras)):
+ print(palavras)
+for palavra in palavras:
+ print(palavra)
+  
+ import random
+  
+Lista_de_palavras = ['barça','city','inter','juve','milan']
+palavra_escolhida = random.choice(Lista_de_palavras)
+
+schwab = input("digite uma letra pobre ")
+x = 2
+while x < 10:
+
+  
+  
